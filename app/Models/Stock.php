9a2cedeb6fcde3b0e2fec112704131f82a21b3b5
Jhonny Ribeiro <jaribeiro32@gmail.com>
2021-01-06 10:42:24 -0300

    Product Tracking
    php artisan make:command TrackerCommand

--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Http;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,30 @@ class Stock extends Model
     use HasFactory;
 
     protected $table = 'stock';
+
+    protected $casts
+        = [
+            'in_stock' => 'boolean',
+        ];
+
+    public function track()
+    {
+        // Hia a APi endpoint for associate retailer
+        if ($this->retailer->name == 'Best Buy') {
+
+            //fetch the up-to-date details from the item
+            $results = Http::get('https://foo.test')->json();
+
+            // and then refresh the current stock record
+            $this->update([
+                'in_stock' => $results['available'],
+                'price'    => $results['price'],
+            ]);
+        }
+    }
+
+    public function retailer()
+    {
+        return $this->belongsTo(Retailer::class);
+    }
 }
